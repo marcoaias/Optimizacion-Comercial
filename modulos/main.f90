f66d35158f0c28a80feacb1b5b35f9e46e2984f2
marcoaias <marcodeavula@gmail.com>
2021-04-21 11:34:15 +0200

    probabilidad acumulada graph

--- modulos/main.f90
+++ modulos/main.f90
@@ -2,7 +2,28 @@ program main
   use lib
   implicit none
 
-  integer :: A(3,3)
+  integer :: A(3,3), n, i
+  real(8) :: t, mu, sigma, h, array(201, 2)
+
+  n = 200
+  mu = 0.0
+  sigma = 1.0
+
+  h = 3*2*sigma/dble(n)
+
+
+
+  do i = 0, n
+    t = -3*sigma + i*h
+
+
+
+    array(i+1, 1) = t
+    array(i+1, 2) = probabilidad(t, mu, sigma)
+
+    print *, i, array(i+1, 1), array(i+1, 2)
+
+  end do
 
 
   A(1,:) = (/1,2,3/)
@@ -12,5 +33,15 @@ program main
   call display_integer_matrix(A,3)
 
   print *, probabilidad(dble(0), dble(1), dble(2))
+
+
+
+  ! output data into a file
+   open(1, file = 'data1.dat', status = 'new')
+   do i=1,n+1
+      write(1,*) array(i,1), array(i,2)
+   end do
+
+   close(1)
 
 end program
